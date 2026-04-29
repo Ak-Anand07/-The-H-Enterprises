280d3833cdfd1635679daec214292fc68c2d7575
Ak-Anand07 <anandhasayanan1@gmail.com>
2026-04-30 01:00:32 +0530

    fix: relax configuration schema for OAuth origins

--- api/src/configuration.ts
+++ api/src/configuration.ts
@@ -10,6 +10,17 @@ export const configurationSchema = Type.Intersect([
     port: Type.Number(),
     public: Type.String(),
     origins: Type.Union([Type.Array(Type.String()), Type.String()]),
+    authentication: Type.Object({
+      secret: Type.String(),
+      oauth: Type.Object({
+        redirect: Type.String(),
+        origins: Type.Union([Type.Array(Type.String()), Type.String()]),
+        google: Type.Object({
+          key: Type.String(),
+          secret: Type.String()
+        })
+      })
+    }),
     mysql: Type.Any()
   })
 ])
